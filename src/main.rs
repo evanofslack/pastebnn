14f1ba2a16317dda9ca6dde08cc37e2fd78b4999
evan slack <evan.slack@tufts.edu>
2022-09-16 17:11:43 -0400

    add axum starter

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+// cargo run -p example-hello-world
+
+use axum::{response::Html, routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(handler));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>welcome to raste!</h1>")
+}
