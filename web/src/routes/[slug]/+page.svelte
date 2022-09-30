e0156211c9550893fb9c082ad61ab0277437cce1
evan slack <evan.slack@tufts.edu>
2022-09-29 20:28:41 -0400

    wtf icloud

--- web/src/routes/[slug]/+page.svelte
+++ web/src/routes/[slug]/+page.svelte
@@ -8,5 +8,6 @@
     let text = paste.text
 </script>
 
-<h1 class="text-neutral-300 pl-2 pb-3">{paste.key}</h1>
+<h1 class="font-mono text-neutral-300/70 pl-2 pb-3">{paste.key}</h1>
+<p></p>
 <textarea class="w-full h-64 border p-2 bg-zinc-800 border-zinc-700 text-neutral-300 placeholder-neutral-300/30 focus:border-zinc-600 focus:outline-none" bind:value={text}></textarea>
