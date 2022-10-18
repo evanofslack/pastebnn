6b3a749e96cc03af84f9a9c3a811e21deb01cc3a
evan slack <evan.slack@tufts.edu>
2022-10-18 19:58:26 -0400

    move handlers into mod

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-use axum::{routing::{get, post, delete},
-    http::StatusCode,
-    response::IntoResponse,
-    extract::{Extension, Path},
-    Json, Router
+use axum::{
+    extract::Extension,
+    Router
 };
 use std::{
     net::SocketAddr,
@@ -19,6 +17,7 @@ use tokio::time;
 
 mod models;
 mod db;
+mod handler;
 
 #[tokio::main]
 async fn main() {
@@ -56,64 +55,11 @@ async fn main() {
 
 fn create_app(storer: DynStorer) -> Router {
 
-    let app: Router = Router::new()
-        .route("/hello", get(root))
-        .route("/api/paste", post(create_paste))
-        .route("/api/paste/:key", get(find_paste))
-        .route("/api/paste/:key", delete(delete_paste))
-        .route("/api/clean", get(delete_expired))
+    let app = handler::routes()
         .layer(Extension(storer))
         .layer(CorsLayer::permissive())
         .layer(TraceLayer::new_for_http());
     return app
-}
-
-async fn root() -> &'static str {
-    "hello world"
-}
-
-async fn create_paste(
-    Json(payload): Json<models::CreatePaste>,
-    Extension(state): Extension<DynStorer>,
-) -> Result<impl IntoResponse, StatusCode > {
-
-    let paste = models::Paste::new(payload.key, payload.text, payload.seconds_until_expire);
-    if let Ok(paste) = state.create(paste.clone()).await {
-        return Ok((StatusCode::CREATED, Json(paste)))
-    } else {
-        return Err(StatusCode::INTERNAL_SERVER_ERROR);
-    }
-}
-
-async fn find_paste(
-    Path(key): Path<String>,
-    Extension(state): Extension<DynStorer>,
-) -> Result<impl IntoResponse, StatusCode>  {
-    
-    if let Ok(paste) = state.get(key).await {
-        return Ok(Json(paste.clone()));
-
-    } else {
-        return Err(StatusCode::NOT_FOUND);
-    }
-}
-
-async fn delete_paste(
-    Path(key): Path<String>,
-    Extension(state): Extension<DynStorer>,
-) -> Result<impl IntoResponse, StatusCode>  {
-
-    if let Ok(paste) = state.delete(&key).await {
-        return Ok(Json(paste.clone()));
-
-    } else {
-        return Err(StatusCode::NOT_FOUND);
-    }
-}
-
-async fn delete_expired(Extension(state): Extension<DynStorer>) -> Result<impl IntoResponse, StatusCode> {
-    remove_expired(state).await.expect("no err currently");
-    return Ok(StatusCode::OK)
 }
 
 async fn remove_expired(storer: DynStorer) -> Result<(), Error> {
