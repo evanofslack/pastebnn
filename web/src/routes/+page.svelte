b4e6a8fa2c803c1d24c39e3cbf99c6f5688d29fc
evan slack <evan.slack@outlook.com>
2022-10-21 01:32:32 -0400

    improved expiration dropdown

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,34 +1,23 @@
-
 <script lang="ts">
     import { goto } from '$app/navigation';
     import type {Paste, CreatePaste} from "../../interfaces"
     import {DISPLAY_URL} from "../constants"
     import Select from 'svelte-select';
 
-
     const expire_times = [
-        {value: 10, label: '10 seconds'},
-        {value: 60,label: '1 minute'},
-        {value: 600,label: '10 minutes'},
+        {value: 600,label: '10 min'},
         {value: 3600,label: '1 hour'},
         {value: 24 * 3600,label: '1 day'},
-        {value: -1, label: 'Never'},
+        {value: 24 * 3600,label: '1 week'},
+        {value: -1, label: 'never'},
     ];
-    const default_expire = expire_times[1]
+    const default_expire = expire_times[0]
 
     let text = ""
     let key = ""
     let expires = default_expire
     let burn = false
-    let placeholder = "big-fat-lobster"
-
-    function handleSelect(event:any) {
-		expires = event.detail;
-	}
-	
-	function handleClear() {
-		expires = default_expire;
-	}
+    let placeholder = randomKey()
 
     async function handleSubmit() {
         let baseURL = "http://localhost:8080/api/paste"
@@ -46,19 +35,45 @@
 
         // redirect to paste url
         goto(`/${paste.key}`, { replaceState: false }) 
-
 	}
+
+    function randomKey(): string {
+        let adjectives = ["young", "old", "small", "large", "cute", "ugly", "sly", "overt", "loud", "quiet", "crazy", "tame", "smart", "dumb", "lazy", "weak", "strong", "fast", "slow", "fat", "thin", "hot", "cold", "wild", "tame", "wet", "dry", "rich", "poor", "sad", "happy"]
+        let nouns = ["lion", "trout", "ant", "hawk", "moose", "swan", "goat", "slug", "mole", "toad", "wolf", "crab","ox","seal", "fox", "moth", "worm", "yak", "loon"]
+        const randAdj = Math.floor(Math.random() * adjectives.length);
+        const randNoun = Math.floor(Math.random() * nouns.length);
+        return adjectives[randAdj] + "-" + nouns[randNoun]
+    }
 </script>
 
 <div class="flex flex-row mb-4">
+    <!-- key input -->
     <div class="border-y border-l bg-zinc-800 text-neutral-400 py-2 pl-2 border-zinc-700">{DISPLAY_URL + "/"}</div>
     <input class="mr-4 border-y border-r py-2 pr-2 bg-zinc-800 border-zinc-700 text-neutral-300 placeholder-neutral-300/20 focus:outline-none" bind:value={key} placeholder={placeholder}/>
-    <form class="w-48 flex">
-        <label for="expire">Expires:</label>
-        <Select id="expire" items={expire_times} on:select={handleSelect} on:clear={handleClear}></Select>
-    </form>
+
+    <!-- expiration dropdown-->
+    <div class="flex items-center mr-4">
+        <p class="text-neutral-300/60 mr-1">expiration</p>
+        <select class="p-2" value={expires} placeholder={default_expire.label}>
+            {#each expire_times as time}
+                <option value={time}>
+                    {time.label}
+                </option>
+            {/each}
+        </select>
+    </div>
+
+    <!-- burn button -->
+    <label class="pr-4 flex items-center">
+        <p class="text-neutral-300/60 pr-1">burn on read</p>
+        <input type=checkbox bind:checked={burn}>
+    </label>
+
+    <!-- submit button -->
     <div>
         <button class="p-2 bg-sky-900/60 border border-sky-900/90 text-neutral-300 hover:text-neutral-300 hover:bg-sky-500/30 hover:border-sky-300/40" on:click={handleSubmit} >submit</button>
     </div>
 </div>
-<textarea placeholder="enter you paste..." class="w-full h-64 border p-2 bg-zinc-800 border-zinc-700 text-neutral-300 placeholder-neutral-300/20 focus:border-zinc-600 focus:outline-none" bind:value={text}></textarea>
+
+<!-- text area -->
+<textarea placeholder="enter you paste..." class="w-full h-2/3 border p-2 bg-zinc-800 border-zinc-700 text-neutral-300 placeholder-neutral-300/20 focus:border-zinc-600 focus:outline-none" bind:value={text}></textarea>
--- web/src/routes/[slug]/+page.svelte
+++ web/src/routes/[slug]/+page.svelte
@@ -17,6 +17,6 @@
 
 <h1 class="flex justify-between w-full">
     <h1 class="font-mono text-neutral-300/70 pl-2 pb-3">{paste.key}</h1>
-    <p class="text-neutral-300/40">expires in {expires_in} seconds</p>
+    <p class="text-neutral-300/40">expires in ~{expires_in} sec</p>
 </h1>
 <textarea class="w-full h-64 border p-2 bg-zinc-800 border-zinc-700 text-neutral-300 placeholder-neutral-300/30 focus:border-zinc-600 focus:outline-none" bind:value={text}></textarea>
