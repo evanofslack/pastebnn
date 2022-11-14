c56b9a33b75d9b5370a0b0f3bd0fcbbd81983a0a
evanofslack <evan.slack@outlook.com>
2022-11-14 01:14:03 -0500

    implement expiration of key/value pairs

--- src/db/redis.rs
+++ src/db/redis.rs
@@ -27,9 +27,12 @@ impl Redis {
 #[async_trait]
 impl Storer for Redis {
     async fn get(&self, key: String) -> Result<models::Paste, &'static str> {
+        let found_paste: models::Paste;
         match self.conn.clone().get::<_, String>(key).await {
-            Ok(paste_str) => match serde_json::from_str(&paste_str) {
-                Ok(paste) => Ok(paste),
+            Ok(paste_str) => match serde_json::from_str::<models::Paste>(&paste_str) {
+                Ok(paste) => {
+                    found_paste = paste;
+                }
                 Err(err) => {
                     println!("{}", err);
                     return Err("Parse to JSON Failed");
@@ -37,70 +40,71 @@ impl Storer for Redis {
             },
             Err(err) => {
                 println!("{}", err);
+                return Err("paste not found");
+            }
+        }
+        if found_paste.burn_on_read {
+            self.delete(&found_paste.key).await?
+        }
+        Ok(found_paste)
+    }
+
+    async fn create(&self, paste: models::Paste) -> Result<(), &'static str> {
+        let paste_str = serde_json::to_string(&paste).unwrap();
+        let mut conn = self.conn.clone();
+
+        // if paste has expiration, use redis command SETEX to set key with expiration
+        // if paste does not have expiration, use redis command SET to set key normally
+        match paste.expires {
+            Some(time) => {
+                match conn
+                    .set_ex::<String, String, String>(
+                        paste.key,
+                        paste_str,
+                        time.try_into().unwrap(),
+                    )
+                    .await
+                {
+                    Ok(_) => Ok(()),
+                    Err(err) => {
+                        println!("{}", err);
+                        Err("error creating paste in redis store")
+                    }
+                }
+            }
+            None => {
+                match conn
+                    .set::<String, String, String>(paste.key.clone(), paste_str)
+                    .await
+                {
+                    Ok(_) => Ok(()),
+                    Err(err) => {
+                        println!("{}", err);
+                        Err("error creating paste in redis store")
+                    }
+                }
+            }
+        }
+    }
+
+    async fn delete(&self, key: &String) -> Result<(), &'static str> {
+        match self.conn.clone().del::<String, i32>(key.to_string()).await {
+            Ok(_) => Ok(()),
+            Err(err) => {
+                println!("{}", err);
                 Err("paste not found")
             }
         }
     }
-    // let found_paste: models::Paste;
-    // if let Some(paste) = self.db.read().unwrap().get(&key) {
-    //     found_paste = paste.clone();
-    // } else {
-    //     return Err("paste not found");
-    // }
-    // if found_paste.burn_on_read {
-    //     self.delete(&found_paste.key).await?;
-    // }
-    // return Ok(found_paste);
 
-    async fn create(&self, paste: models::Paste) -> Result<(), &'static str> {
-        let str_paste = serde_json::to_string(&paste).unwrap();
-        match self
-            .conn
-            .clone()
-            .set::<_, _, String>(paste.key.clone(), str_paste)
-            .await
-        {
-            Ok(_) => Ok(()),
-            Err(err) => {
-                println!("{}", err);
-                Err("error creating paste in redis store")
-            }
-        }
-    }
-
-    async fn delete(&self, key: &String) -> Result<models::Paste, &'static str> {
-        // if let Some(paste) = self.db.write().unwrap().remove(key) {
-        //     return Ok(paste);
-        // } else {
-        //     return Err("paste not found");
-        // }
-        unimplemented!();
-    }
     async fn get_expired(&self) -> Vec<models::Paste> {
-        // let now = SystemTime::now()
-        //     .duration_since(UNIX_EPOCH)
-        //     .expect("Time went backwards")
-        //     .as_millis();
-
-        // let mut expired: Vec<models::Paste> = Vec::new();
-
-        // for (_, paste) in self.db.read().unwrap().iter() {
-        //     if let Some(expires) = paste.expires {
-        //         if expires <= now {
-        //             expired.push(paste.to_owned())
-        //         }
-        //     }
-        // }
-        // return expired;
-        unimplemented!();
+        // redis key/values pairs expire natively if set with SETEX
+        let empty: Vec<models::Paste> = vec![];
+        return empty;
     }
     async fn delete_expired(&self) -> Result<(), &'static str> {
-        // let expired = self.get_expired().await;
-        // for paste in expired.iter() {
-        //     self.delete(&paste.key).await?;
-        // }
-        // return Ok(());
-        unimplemented!();
+        // redis key/values pairs expire natively if set with SETEX
+        Ok(())
     }
 }
 
@@ -123,44 +127,42 @@ mod tests {
         assert_eq!(paste, resp);
     }
 
-    //     #[tokio::test]
-    //     async fn delete() {
-    //         let db = InMemory::default();
-    //         let paste = models::Paste::new(String::from("key"), String::from("text"), None, false);
-    //         db.create(paste.clone())
-    //             .await
-    //             .expect("should create new paste");
+    #[tokio::test]
+    async fn delete() {
+        let db = Redis::new().await.unwrap();
+        let paste = models::Paste::new(String::from("key"), String::from("text"), None, false);
+        db.create(paste.clone())
+            .await
+            .expect("should create new paste");
 
-    //         let resp = db
-    //             .delete(&paste.clone().key)
-    //             .await
-    //             .expect("should return paste");
-    //         assert_eq!(paste, resp);
+        db.delete(&paste.clone().key)
+            .await
+            .expect("should delete paste");
 
-    //         let resp = db.get(paste.clone().key).await;
-    //         assert!(resp.is_err());
-    //         if let Err(msg) = resp {
-    //             assert_eq!(msg, "paste not found")
-    //         }
-    //     }
+        let resp = db.get(paste.clone().key).await;
+        assert!(resp.is_err());
+        if let Err(msg) = resp {
+            assert_eq!(msg, "paste not found")
+        }
+    }
 
-    //     #[tokio::test]
-    //     async fn burn_on_read() {
-    //         let db = InMemory::default();
-    //         let paste = models::Paste::new(String::from("key"), String::from("text"), None, true);
-    //         db.create(paste.clone())
-    //             .await
-    //             .expect("should create new paste");
-    //         let resp = db
-    //             .get(paste.clone().key)
-    //             .await
-    //             .expect("should return paste");
-    //         assert_eq!(paste, resp);
+    #[tokio::test]
+    async fn burn_on_read() {
+        let db = Redis::new().await.unwrap();
+        let paste = models::Paste::new(String::from("key"), String::from("text"), None, true);
+        db.create(paste.clone())
+            .await
+            .expect("should create new paste");
+        let resp = db
+            .get(paste.clone().key)
+            .await
+            .expect("should return paste");
+        assert_eq!(paste, resp);
 
-    //         let resp = db.get(paste.clone().key).await;
-    //         assert!(resp.is_err());
-    //         if let Err(msg) = resp {
-    //             assert_eq!(msg, "paste not found")
-    //         }
-    //     }
+        let resp = db.get(paste.clone().key).await;
+        assert!(resp.is_err());
+        if let Err(msg) = resp {
+            assert_eq!(msg, "paste not found")
+        }
+    }
 }
