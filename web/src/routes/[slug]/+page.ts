6da653097ceb6557c01302157638d35b2afdcff4
evanofslack <evan.slack@outlook.com>
2023-01-16 22:37:13 -0500

    use env vars for frontend

--- web/src/routes/[slug]/+page.ts
+++ web/src/routes/[slug]/+page.ts
@@ -1,10 +1,10 @@
 import type { LoadEvent } from '@sveltejs/kit';
 import type { Paste } from '../../../interfaces';
-import { APP_HOST, APP_PORT } from '$env/static/public';
+import { env } from '$env/dynamic/public';
 
 export async function load({ fetch, params }: LoadEvent) {
 	const pasteID = params.slug;
-	const endpoint = `http://${APP_HOST}:${APP_PORT}/api/paste/${pasteID}`;
+	const endpoint = `http://${env.SERVER_HOST}:${env.SERVER_PORT}/api/paste/${pasteID}`;
 	const res = await fetch(endpoint);
 	const paste: Paste = await res.json();
 	return { paste: paste };
