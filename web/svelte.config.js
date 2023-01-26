3cd72cb1040d89c9bc67105f7c5360e6edaf5ccb
evanofslack <evan.slack@outlook.com>
2023-01-25 23:08:08 -0500

    update deps

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,26 +1,16 @@
-// import adapter from '@sveltejs/adapter-auto'
-// import adapter from '@sveltejs/adapter-static'
-import adapter from '@sveltejs/adapter-node'
-import preprocess from 'svelte-preprocess'
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
+export default {
 	preprocess: [
 		preprocess({
 			postcss: true
 		})
 	],
-
 	kit: {
 		adapter: adapter({
 			fallback: '200.html'
 		}),
-		env: {
-			publicPrefix: ''
-		}
+		prerender: { entries: [] }
 	}
-}
-
-export default config
+};
