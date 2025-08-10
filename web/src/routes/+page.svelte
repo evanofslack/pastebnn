eb45a28d614b3bedc4ec9ad649a4e842e510883b
evan slack <evan.slack@outlook.com>
2025-08-10 15:23:29 -0400

    relative routes

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,97 +1,164 @@
 <script lang="ts">
-    import { goto } from '$app/navigation';
-    import { onMount } from 'svelte';
-    import type {CreatePaste} from "../../interfaces"
-    import { PUBLIC_API_BASE_URL } from '$env/static/public'
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
+	import type { CreatePaste } from '../../interfaces';
 
-    // set base URL if serving from dev server
-    const api_base_url = PUBLIC_API_BASE_URL || ""
-    let app_name = "localhost"
+	let app_name = 'localhost';
 
-    onMount(async () => {
-        // Fetch app name from backend on mount
-        try {
-            const res = await fetch(`${api_base_url}/api/config`);
-            const config = await res.json();
-            app_name = config.app_url;
-        } catch (e) {
-        }
-    });
+	onMount(async () => {
+		// Fetch app name from backend on mount
+		try {
+			const res = await fetch('/api/config');
+			const config = await res.json();
+			app_name = config.app_url;
+		} catch (e) {}
+	});
 
-    const expire_times = [
-        {value: 600,label: '10 min'},
-        {value: 3600,label: '1 hour'},
-        {value: 24 * 3600,label: '1 day'},
-        {value: 24 * 3600 * 7,label: '1 week'},
-        {value: 24 * 3600 * 30,label: '1 month'},
-        {value: null, label: 'never'},
-    ];
-    const default_expire = expire_times[0]
+	const expire_times = [
+		{ value: 600, label: '10 min' },
+		{ value: 3600, label: '1 hour' },
+		{ value: 24 * 3600, label: '1 day' },
+		{ value: 24 * 3600 * 7, label: '1 week' },
+		{ value: 24 * 3600 * 30, label: '1 month' },
+		{ value: null, label: 'never' }
+	];
+	const default_expire = expire_times[0];
 
-    let text = ""
-    let key = ""
-    let expires = default_expire
-    let burn = false
-    let placeholder = randomKey()
+	let text = '';
+	let key = '';
+	let expires = default_expire;
+	let burn = false;
+	let placeholder = randomKey();
 
-    async function handleSubmit() {
+	async function handleSubmit() {
+		let endpoint = `/api/paste`;
+		let paste: CreatePaste = {
+			text: text,
+			key: key ? key : placeholder,
+			seconds_until_expire: expires.value,
+			burn_on_read: burn
+		};
+		let requestOptions = {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(paste)
+		};
 
-        let endpoint = `${api_base_url}/api/paste`
-        let paste: CreatePaste = {text: text, key: key ? key : placeholder, seconds_until_expire: expires.value, burn_on_read: burn};
-        let requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(paste)
-        };
+		let res = await fetch(endpoint, requestOptions);
 
-        let res = await fetch(endpoint, requestOptions);
-
-        // redirect to paste url
-        goto(`/${paste.key}`, { replaceState: false })
+		// redirect to paste url
+		goto(`/${paste.key}`, { replaceState: false });
 	}
 
-    function randomKey(): string {
-        let adjectives = ["young", "old", "small", "large", "cute", "ugly", "sly", "overt", "loud", "quiet", "crazy", "tame", "smart", "dumb", "lazy", "weak", "strong", "fast", "slow", "fat", "thin", "hot", "cold", "wild", "tame", "wet", "dry", "rich", "poor", "sad", "happy", "rowdy"]
-        let nouns = ["lion", "trout", "ant", "hawk", "moose", "swan", "goat", "slug", "mole", "toad", "wolf", "crab","ox","seal", "fox", "moth", "worm", "yak", "loon", "deer", "hen"]
-        const randAdj = Math.floor(Math.random() * adjectives.length);
-        const randNoun = Math.floor(Math.random() * nouns.length);
-        return adjectives[randAdj] + "-" + nouns[randNoun]
-    }
+	function randomKey(): string {
+		let adjectives = [
+			'young',
+			'old',
+			'small',
+			'large',
+			'cute',
+			'ugly',
+			'sly',
+			'overt',
+			'loud',
+			'quiet',
+			'crazy',
+			'tame',
+			'smart',
+			'dumb',
+			'lazy',
+			'weak',
+			'strong',
+			'fast',
+			'slow',
+			'fat',
+			'thin',
+			'hot',
+			'cold',
+			'wild',
+			'tame',
+			'wet',
+			'dry',
+			'rich',
+			'poor',
+			'sad',
+			'happy',
+			'rowdy'
+		];
+		let nouns = [
+			'lion',
+			'trout',
+			'ant',
+			'hawk',
+			'moose',
+			'swan',
+			'goat',
+			'slug',
+			'mole',
+			'toad',
+			'wolf',
+			'crab',
+			'ox',
+			'seal',
+			'fox',
+			'moth',
+			'worm',
+			'yak',
+			'loon',
+			'deer',
+			'hen'
+		];
+		const randAdj = Math.floor(Math.random() * adjectives.length);
+		const randNoun = Math.floor(Math.random() * nouns.length);
+		return adjectives[randAdj] + '-' + nouns[randNoun];
+	}
 </script>
 
 <div class="flex flex-row mb-4 justify-between">
-    <!-- key input -->
-    <div class="flex flex-row">
-        <div class="border-y border-l bg-zinc-800 text-neutral-400 py-2 pl-2 border-zinc-700">{app_name + "/"}</div>
-        <input class="mr-4 border-y border-r py-2 pr-2 bg-zinc-800 border-zinc-700 text-neutral-300 placeholder-neutral-300/20 focus:outline-none" bind:value={key} placeholder={placeholder}/>
-    </div>
+	<!-- key input -->
+	<div class="flex flex-row">
+		<div class="border-y border-l bg-zinc-800 text-neutral-400 py-2 pl-2 border-zinc-700">
+			{app_name + '/'}
+		</div>
+		<input
+			class="mr-4 border-y border-r py-2 pr-2 bg-zinc-800 border-zinc-700 text-neutral-300 placeholder-neutral-300/20 focus:outline-none"
+			bind:value={key}
+			{placeholder}
+		/>
+	</div>
 
-    <div class="flex flex-row">
+	<div class="flex flex-row">
+		<!-- burn button -->
+		<label class="mr-4 flex items-center">
+			<p class="text-neutral-300/60 pr-1">burn</p>
+			<input class="w-5 h-5 " type="checkbox" bind:checked={burn} />
+		</label>
 
-        <!-- burn button -->
-        <label class="mr-4 flex items-center">
-            <p class="text-neutral-300/60 pr-1">burn</p>
-            <input class = "w-5 h-5 " type=checkbox bind:checked={burn}>
-        </label>
+		<!-- expiration dropdown-->
+		<div class="flex items-center mr-4">
+			<p class="text-neutral-300/60 mr-1">expire</p>
+			<select class="p-2" bind:value={expires} placeholder={default_expire.label}>
+				{#each expire_times as time}
+					<option value={time}>
+						{time.label}
+					</option>
+				{/each}
+			</select>
+		</div>
 
-        <!-- expiration dropdown-->
-        <div class="flex items-center mr-4">
-            <p class="text-neutral-300/60 mr-1">expire</p>
-            <select class="p-2" bind:value={expires} placeholder={default_expire.label}>
-                {#each expire_times as time}
-                    <option value={time}>
-                        {time.label}
-                    </option>
-                {/each}
-            </select>
-        </div>
-
-        <!-- submit button -->
-        <div>
-            <button class="p-2 ml-2 bg-sky-900/60 border border-sky-900/90 text-neutral-300 hover:text-neutral-300 hover:bg-sky-500/30 hover:border-sky-300/40" on:click={handleSubmit} >submit</button>
-        </div>
-    </div>
+		<!-- submit button -->
+		<div>
+			<button
+				class="p-2 ml-2 bg-sky-900/60 border border-sky-900/90 text-neutral-300 hover:text-neutral-300 hover:bg-sky-500/30 hover:border-sky-300/40"
+				on:click={handleSubmit}>submit</button
+			>
+		</div>
+	</div>
 </div>
 
 <!-- text area -->
-<textarea placeholder="enter you paste..." class="w-full h-2/3 border p-2 bg-zinc-800 border-zinc-700 text-neutral-300 placeholder-neutral-300/20 focus:border-zinc-600 focus:outline-none" bind:value={text}></textarea>
+<textarea
+	placeholder="enter you paste..."
+	class="w-full h-2/3 border p-2 bg-zinc-800 border-zinc-700 text-neutral-300 placeholder-neutral-300/20 focus:border-zinc-600 focus:outline-none"
+	bind:value={text}
+/>
--- web/src/routes/[slug]/+page.svelte
+++ web/src/routes/[slug]/+page.svelte
@@ -1,85 +1,78 @@
-<script lang='ts'>
-    import type { Paste } from '../../../interfaces';
-    import { PUBLIC_API_BASE_URL } from '$env/static/public';
-    import { onMount } from 'svelte';
-    import { goto } from '$app/navigation';
-    import MdDelete from 'svelte-icons/md/MdDelete.svelte'
-	import MdContentCopy from 'svelte-icons/md/MdContentCopy.svelte'
+<script lang="ts">
+	import type { Paste } from '../../../interfaces';
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+	import MdDelete from 'svelte-icons/md/MdDelete.svelte';
+	import MdContentCopy from 'svelte-icons/md/MdContentCopy.svelte';
 
+	// convert paste expiration to alternative time unit for display
+	function formatExpires(time: number): string {
+		if (time == null) {
+			return 'never expires';
+		}
 
-    // convert paste expiration to alternative time unit for display
-    function formatExpires(time:number): string {
-
-        if (time == null) {
-            return "never expires"
-        }
-
-        let seconds_left =  Math.floor((time - Date.now()) / 1000)
-        switch(true) {
-            case (seconds_left < 0):
-                return "expired";
-            case (seconds_left < 60):
-                return `expires in ~${seconds_left} secs`;
-            case (seconds_left < 60 * 60):
-                return `expires in ~${Math.floor(seconds_left / 60)} mins`;
-            case (seconds_left < 60 * 60 * 24):
-                return `expires in ~${Math.floor(seconds_left / (60 * 60))} hours`;
-            default:
-                return `expires in ~${Math.floor(seconds_left / (60 * 60 * 24))} days`;
-        }
-    }
-
-
-
-    /** @type {import('./$types').PageData} */
-    export let data;
-    // get the pasteID slug from the URL
-    const pasteID: string = data.pasteID;
-
-    // set base URL if serving from dev server
-    const api_base_url = PUBLIC_API_BASE_URL || ""
-
-    // mutable data, waiting for server response
-    let key = "";
-    let text = "";
-    let expiration = "";
-
-    onMount(async () => {
-        const endpoint = `${api_base_url}/api/paste/${pasteID}`
-        const res = await fetch(endpoint)
-        let paste = await res.json()
-
-        key = paste.key
-        text = paste.text
-        expiration = formatExpires(paste.expires)
-
-    });
-
-    async function handleDelete() {
-        const endpoint = `${api_base_url}/api/paste/${pasteID}`
-        let requestOptions = {
-            method: 'DELETE',
-            headers: {
-                'Content-type': 'application/json'
-            }
-        };
-        let res = await fetch(endpoint, requestOptions);
-        goto(`/`, { replaceState: false })
+		let seconds_left = Math.floor((time - Date.now()) / 1000);
+		switch (true) {
+			case seconds_left < 0:
+				return 'expired';
+			case seconds_left < 60:
+				return `expires in ~${seconds_left} secs`;
+			case seconds_left < 60 * 60:
+				return `expires in ~${Math.floor(seconds_left / 60)} mins`;
+			case seconds_left < 60 * 60 * 24:
+				return `expires in ~${Math.floor(seconds_left / (60 * 60))} hours`;
+			default:
+				return `expires in ~${Math.floor(seconds_left / (60 * 60 * 24))} days`;
+		}
 	}
 
+	/** @type {import('./$types').PageData} */
+	export let data;
+	// get the pasteID slug from the URL
+	const pasteID: string = data.pasteID;
 
+	// mutable data, waiting for server response
+	let key = '';
+	let text = '';
+	let expiration = '';
+
+	onMount(async () => {
+		const endpoint = `/api/paste/${pasteID}`;
+		const res = await fetch(endpoint);
+		let paste = await res.json();
+
+		key = paste.key;
+		text = paste.text;
+		expiration = formatExpires(paste.expires);
+	});
+
+	async function handleDelete() {
+		const endpoint = `/api/paste/${pasteID}`;
+		let requestOptions = {
+			method: 'DELETE',
+			headers: {
+				'Content-type': 'application/json'
+			}
+		};
+		let res = await fetch(endpoint, requestOptions);
+		goto(`/`, { replaceState: false });
+	}
 </script>
 
 <h1 class="flex justify-between w-full">
-    <h1 class="font-mono text-neutral-300/70 pl-2 pb-3">{key}</h1>
-    <div class="flex flex-row content-end">
-        <p class="mb-3 text-neutral-300/40">{expiration}</p>
-        <button class="mb-3 ml-4 w-5 h-5 text-neutral-300/80">
-            <MdContentCopy />
-        </button>
-        <button class="mb-3 ml-2 w-6 h-6 text-neutral-300/80" on:click={handleDelete} >
-            <MdDelete />
-        </button>
-    <div>
+	<h1 class="font-mono text-neutral-300/70 pl-2 pb-3">{key}</h1>
+	<div class="flex flex-row content-end">
+		<p class="mb-3 text-neutral-300/40">{expiration}</p>
+		<button class="mb-3 ml-4 w-5 h-5 text-neutral-300/80">
+			<MdContentCopy />
+		</button>
+		<button class="mb-3 ml-2 w-6 h-6 text-neutral-300/80" on:click={handleDelete}>
+			<MdDelete />
+		</button>
+		<div />
+	</div>
 </h1>
-<textarea class="w-full h-2/3 h-64 border p-2 bg-zinc-800 border-zinc-700 text-neutral-300 placeholder-neutral-300/30 focus:border-zinc-600 focus:outline-none" bind:value={text}></textarea>
+<textarea
+	class="w-full h-2/3 h-64 border p-2 bg-zinc-800 border-zinc-700 text-neutral-300 placeholder-neutral-300/30 focus:border-zinc-600 focus:outline-none"
+	bind:value={text}
+/>
